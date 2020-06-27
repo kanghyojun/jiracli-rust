2f4e7a6dae53db72c1c32f4821d7d2970b284d49
Kang Hyojun <iam.kanghyojun@gmail.com>
2020-06-28 00:39:18 +0900

    Add CLI interface
    Signed-off-by: Kang Hyojun <iam.kanghyojun@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use clap::{App, Arg, SubCommand};
+
 mod app_config;
 mod http;
 
@@ -5,13 +7,25 @@ use app_config::{Config, JsonData, Token};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = App::new("Jira CLI")
+        .version("1.0")
+        .subcommand(
+            SubCommand::with_name("issue")
+                .about("Get issue with key")
+                .arg(Arg::with_name("KEY").help("Jira Issue key. e.g. JIRA-1")),
+        )
+        .get_matches();
     let transport = http::HttpTransport {
         config: Config::from_path()?,
         token: Token::from_path()?,
     };
     let jira_client = http::JiraClient { transport };
-    let resp = jira_client.get_issue("SOA-402").await?;
 
-    println!("{:#?}", resp);
+    if let Some(matches) = matches.subcommand_matches("issue") {
+        let key = matches.value_of("KEY").unwrap();
+        let resp = jira_client.get_issue(key).await?;
+        println!("{:#?}", resp);
+    }
+
     Ok(())
 }
