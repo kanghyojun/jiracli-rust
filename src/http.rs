b01cb95e26d65c5626f9acae7b151f0b8515956a
Kang Hyojun <iam.kanghyojun@gmail.com>
2020-06-27 23:35:07 +0900

    Make transport layer to test JiraClient
    Signed-off-by: Kang Hyojun <iam.kanghyojun@gmail.com>

--- src/http.rs
+++ src/http.rs
@@ -1,15 +1,25 @@
 use std::error::Error;
 
-use reqwest::{Client, Response, Url};
+use async_trait::async_trait;
+use reqwest::{Client, Url};
 
 use crate::app_config::{Config, Token};
 
-pub struct JiraClient {
+#[async_trait]
+pub trait Transport {
+    fn base_url(&self) -> String;
+
+    async fn req_get(&self, path: &str) -> Result<String, Box<dyn Error>>;
+}
+
+pub struct HttpTransport {
     pub config: Config,
+
     pub token: Token,
 }
 
-impl JiraClient {
+#[async_trait]
+impl Transport for HttpTransport {
     fn base_url(&self) -> String {
         String::from(format!(
             "https://api.atlassian.com/ex/jira/{}/rest/api/3/",
@@ -17,23 +27,32 @@ impl JiraClient {
         ))
     }
 
-    async fn req_get(&self, path: &str) -> Result<Response, Box<dyn Error>> {
+    async fn req_get(&self, path: &str) -> Result<String, Box<dyn Error>> {
         let client = Client::new();
         let url = Url::parse(&self.base_url())?.join(path)?;
         let resp = client
             .get(url.as_str())
             .bearer_auth(&self.token.access_token)
             .send()
+            .await?
+            .text()
             .await?;
 
         Ok(resp)
     }
+}
 
+pub struct JiraClient<T>
+where T: Transport
+{
+    pub transport: T,
+}
+
+impl JiraClient<T>
+where T: Transport {
     pub async fn get_issue(&self, key: &str) -> Result<String, Box<dyn Error>> {
-        let resp = self
+        let resp = self.transport
             .req_get(&format!("./issue/{}", key))
-            .await?
-            .text()
             .await?;
 
         Ok(resp)
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ use std::error::Error;
 mod http;
 mod app_config;
 
-use http::JiraClient;
 use app_config::{Config, Token, JsonData};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let jira_client = JiraClient {
+    let transport = http::HttpTransport {
         config: Config::from_path()?,
         token: Token::from_path()?,
     };
+    let jira_client = http::JiraClient { transport: transport, };
     let resp = jira_client.get_issue("SOA-402").await?;
 
     println!("{:#?}", resp);
